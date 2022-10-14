336fead5e01e1f047387c309692b67f75ccb4d0d
MiguelGarcia23 <alejandro23vzla@gmail.com>
2022-10-14 15:23:00 -0400

    Creando un Servicio para centralizar la data

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,8 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+
 import { Personaje } from '../interfaces/dbz.interface';
+
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
@@ -7,24 +10,15 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-  ];
-
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
     poder: 1000
   }
 
-  agregarNuevoPersonaje( argumento: Personaje ) {
+  /* agregarNuevoPersonaje( argumento: Personaje ) {
     this.personajes.push( argumento );
-  }
+  } */
+
+  constructor() {}
 
 }
